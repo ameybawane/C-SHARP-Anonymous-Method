d9e8de4f3b24b8bac72a0463a26e3de743cabbb4
Amey Bawane <60310813+ameybawane@users.noreply.github.com>
2023-02-09 12:11:45 +0530

    Add files via upload

---
+++ Anonymous_NamelessMethod/Anonymous_NamelessMethod/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anonymous_NamelessMethod
+{
+    internal class Program
+    {
+        public delegate string GreetingsDelegate(string name);
+
+        // named method
+        public static string Greetings(string name)
+        {
+            return "Hello " + name + " a very good morning.";
+        }
+        static void Main(string[] args)
+        {
+            // creating instance of named method
+            // GreetingsDelegate obj = new GreetingsDelegate(Greetings);
+
+            // Anonymous/Nameless Method
+            // start
+            GreetingsDelegate obj = delegate (string name)
+            {
+                return "Hello " + name + " a very good morning.";
+            };
+            // end
+
+            Console.WriteLine(obj.Invoke("Amey"));
+            Console.ReadKey();
+        }
+    }
+}
